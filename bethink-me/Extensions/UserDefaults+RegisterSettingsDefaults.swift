2a28fe320f225bda2c4bb420dd670107259e2904
Nick Pettazzoni <github@n.pettazz.com>
2026-08-11 10:05:23 -0400

    fix missing settings keys in defaults

--- bethink-me/Extensions/UserDefaults+RegisterSettingsDefaults.swift
+++ bethink-me/Extensions/UserDefaults+RegisterSettingsDefaults.swift
@@ -5,6 +5,11 @@ extension UserDefaults {
     static func registerSettingsDefaults() {
         standard.register(defaults: [
             SettingsKey.enableAutocorrect.rawValue: kEnableAutocorrectDefault,
+            SettingsKey.inheritListAlarmsOnImport.rawValue: kInheritListAlarmsOnImportDefault,
+            SettingsKey.enableDedupe.rawValue: kEnableDedupeDefault,
+            SettingsKey.dedupeCaseSensitive.rawValue: kDedupeCaseSensitiveDefault,
+            SettingsKey.dedupeRunOnSync.rawValue: kDedupeRunOnSyncDefault,
+            SettingsKey.dedupeNow.rawValue: false,
             SettingsKey.maxCompletedAgeDays.rawValue: kMaxCompletedAgeDaysDefault,
             SettingsKey.displayNotes.rawValue: kDisplayNotesDefault,
             SettingsKey.displayURLs.rawValue: kDisplayURLsDefault,
